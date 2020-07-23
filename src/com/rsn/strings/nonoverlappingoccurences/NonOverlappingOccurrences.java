a4309ffe29b0b70fde7167e6bc1e70dda98044b8
Rami Serapian <rsn@Ramis-MacBook-Pro.local>
2020-07-23 16:19:55 -0400

    Find non-overlapping occurrences for a pattern in a text

---
+++ src/com/rsn/strings/nonoverlappingoccurences/NonOverlappingOccurrences.java
@@ -0,0 +1,43 @@
+package com.rsn.strings.nonoverlappingoccurences;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class NonOverlappingOccurrences {
+    public static void main(String[] args) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(new FileReader("input2.txt"))) {
+            String pattern = scanner.nextLine();
+            String text = scanner.nextLine();
+
+            List<Integer> result = findAllOccurrences(text, pattern);
+            System.out.println(result.size());
+            result.forEach(index -> System.out.print(index + " "));
+        }
+    }
+
+
+    private static List<Integer> findAllOccurrences(String text, String pattern) {
+        List<Integer> occurrenceIndexes = new ArrayList<>();
+        if (pattern.isEmpty()) {
+            occurrenceIndexes.add(0);
+            return occurrenceIndexes;
+        }
+        if (pattern.length() > text.length()) {
+            return occurrenceIndexes;
+        }
+        int index = 0;
+        do {
+            index = text.indexOf(pattern, index);
+            if (index == -1) {
+                break;
+            } else {
+                occurrenceIndexes.add(index);
+                index += pattern.length();
+            }
+        } while (true);
+        return occurrenceIndexes;
+    }
+}
